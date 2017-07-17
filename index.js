4b5d33d5f661463c695552ce0effe044546da476
gilmae <avocadia@gmail.com>
2017-07-17 22:12:49 +1000

    start turning the image into 25 blobs
    The blobs will be parsed as frequencies…somehow

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var getPixels = require("get-pixels");
 var NodeSynth = require('nodesynth');
+var pad = require('pad-left');
 require('nodesynth/notes');
 
 var notes = [C4, D4, E4, F4, G4, A5, B5, C5]
@@ -13,23 +14,42 @@ getPixels(image, function(err, pixels) {
     console.log("Bad image path")
     return
   }
+
+  var dimensions = pixels.shape;
+  var blob_width = Math.floor(pixels.shape[0]/5);
+  var blob_height = Math.floor(pixels.shape[1]/5);
+
+for (ii=0;ii<5;ii++)
+{
+  for (jj=0;jj<5;jj++)
+  {
+  console.log(pixelatedBlob(pixels, ii, jj,blob_width, blob_height));
+  }
+}
+
+})
+
+function pixelatedBlob(pixels, x,y, width, height) {
   //playPixels(pixels);
   var red = 0;
   var green = 0;
   var blue = 0;
 
-  for (ii=0; ii<250;ii++)
+  var x_start = x*width;
+  var x_end = x_start + width;
+  var y_start = y*height;
+  var y_end = y_start + height;
+
+  for (var xx=x_start; xx<x_end;xx++)
   {
-      for (jj=0;jj<250;jj++) {
-        red += pixels.get(ii,jj,0);
-        green += pixels.get(ii,jj,1);
-        blue += pixels.get(ii,jj,2);
+      for (var yy=y_start;yy<y_end;yy++) {
+        red += pixels.get(xx,yy,0);
+        green += pixels.get(xx,yy,1);
+        blue += pixels.get(xx,yy,2);
       }
   }
-
-  console.log(Math.round(red/62500).toString(16), Math.round(green/62500).toString(16), Math.round(blue/62500).toString(16));
-})
-
+  return pad(Math.round(red/62500).toString(16),2,'0') + pad(Math.round(green/62500).toString(16),2,'0') +  pad(Math.round(blue/62500).toString(16),2,'0');
+}
 
 function playPixels(pixels) {
 
